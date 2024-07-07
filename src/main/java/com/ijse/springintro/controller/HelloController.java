59d07943f4031f1da9ebcd43c54d3109aac96562
Bimsara Walallawita <bwalallawita@gmail.com>
2024-07-07 13:42:17 +0530

    Initial Commit

---
+++ src/main/java/com/ijse/springintro/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.ijse.springintro.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello world";
+    }
+
+    @PostMapping("/hello")
+    public String createHello() {
+        return "POST Request to hello endpoint";
+    }
+
+    @GetMapping("/orders")
+    public String showOrders() {
+        return "Showing Orders";
+    }
+}
